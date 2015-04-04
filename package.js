1d59ea74e17c3d694d32d9008b3ca61e69e6f694
Kelly <copleykj@gmail.com>
2015-04-03 20:57:47 -0400

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:messaging",
     summary: "A social messaging package",
-    version: "0.1.0",
+    version: "0.1.1",
     git:"https://github.com/copleykj/socialize-messaging.git"
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
 
     api.use([
-        "meteor", "mongo", "underscore", "socialize:user-model@0.1.0", "socialize:base-model@0.1.0",
+        "meteor", "mongo", "underscore", "socialize:user-model@0.1.0", "socialize:base-model@0.1.2",
         "socialize:friendships@0.2.0", "copleykj:simple-publish@1.0.4", "aldeed:simple-schema@1.3.0",
         "aldeed:collection2@2.3.2", "matb33:collection-hooks@0.7.9", "meteorhacks:unblock@1.1.0"
     ]);
