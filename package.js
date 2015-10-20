c468429cbf153319408732972570b1ad3d0f6741
copleykj <copleykj@gmail.com>
2015-10-20 17:13:57 -0400

    minor version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:messaging",
     summary: "A social messaging package",
-    version: "0.4.2",
+    version: "0.4.3",
     git:"https://github.com/copleykj/socialize-messaging.git"
 });
 
