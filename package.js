3da4c788ec2961dc039cc5b181183396eaa2547a
Kelly Copley <copleykj@gmail.com>
2017-07-23 09:37:22 -0400

    ecmascript implied by base-model

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
     api.use([
-        'check', 'ecmascript', 'socialize:user-model@1.0.0', 'socialize:user-presence@1.0.0', 'socialize:server-time@1.0.0',
+        'check', 'socialize:user-model@1.0.0', 'socialize:user-presence@1.0.0', 'socialize:server-time@1.0.0',
     ]);
 
     api.mainModule('server.js', 'server');
