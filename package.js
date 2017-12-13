21c6564d69eed8e1f17574f27814b0c936f7836b
StorytellerCZ <storyteller@freedombase.net>
2017-12-13 17:41:19 +0100

    Return coversation publication with participation check

--- package.js
+++ package.js
@@ -10,7 +10,8 @@ Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
     api.use([
-        'check', 'socialize:user-presence@1.0.0',
+        'check',
+        'socialize:user-presence@1.0.0',
         'reywood:publish-composite@1.5.2',
     ]);
 
